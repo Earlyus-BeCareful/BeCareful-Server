06b784ff16bc35ab3a3804a38438768895b24f18
kckc0608 <kckc0608@gmail.com>
2025-02-11 00:46:32 +0900

    feat: init project

---
+++ src/main/java/com/becareful/becarefulserver/BecarefulServerApplication.java
@@ -0,0 +1,13 @@
+package com.becareful.becarefulserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BecarefulServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BecarefulServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/becareful/becarefulserver/BecarefulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.becareful.becarefulserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BecarefulServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
