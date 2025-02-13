084656d61f8113d753fca3270c9c25f33ca69b82
kckc0608 <kckc0608@gmail.com>
2025-02-13 20:39:35 +0900

    docs: api 명세 작성

--- src/main/java/com/becareful/becarefulserver/domain/sms/controller/SmsController.java
+++ src/main/java/com/becareful/becarefulserver/domain/sms/controller/SmsController.java
@@ -10,21 +10,26 @@ import com.becareful.becarefulserver.domain.sms.dto.SmsAuthenticateRequest;
 import com.becareful.becarefulserver.domain.sms.dto.SmsSendRequest;
 import com.becareful.becarefulserver.domain.sms.service.SmsService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/sms")
+@Tag(name = "SMS", description = "SMS 인증 관련 API 입니다.")
 public class SmsController {
 
     private final SmsService smsService;
 
+    @Operation(summary = "인증번호 SMS 전송", description = "6자리 인증번호를 생성하여 주어진 전화번호로 전송합니다.")
     @PostMapping("/send-auth-number")
     public ResponseEntity<Void> sendAuthNumber(@RequestBody SmsSendRequest request) {
         smsService.sendAuthNumber(request);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "인증번호 검증", description = "휴대폰 인증번호를 검증합니다.")
     @PostMapping("/authenticate-number")
     public ResponseEntity<Void> authenticateNumber(@RequestBody SmsAuthenticateRequest request) {
         smsService.authenticateNumber(request);
