dd075231496c33956fae886f0169e92968667d3b
Jiye Ryu <ryujy0716@gmail.com>
2025-02-16 11:37:05 +0900

    사회복지사 회원가입 구현 (#43)
    * 사회복지사 회원가입 구현
    
    * security 예외처리
    
    * fix: 일부 유효성 검사 제약 삭제
    
    ---------
    
    Co-authored-by: kckc0608 <kckc0608@gmail.com>

---
+++ src/main/java/com/becareful/becarefulserver/domain/socialworker/domain/vo/Address.java
@@ -0,0 +1,18 @@
+package com.becareful.becarefulserver.domain.socialworker.domain.vo;
+
+import jakarta.persistence.Embeddable;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Embeddable
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Address {
+
+    String streetAddress;
+    String detailAddress;
+}
---
+++ src/main/java/com/becareful/becarefulserver/domain/socialworker/domain/vo/Rank.java
@@ -0,0 +1,5 @@
+package com.becareful.becarefulserver.domain.socialworker.domain.vo;
+
+public enum Rank {
+    MANAGER, SOCIALWORKER;
+}
