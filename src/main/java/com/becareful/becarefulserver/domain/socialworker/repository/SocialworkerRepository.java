c5baf7843401ecefb8e6a050d5f896562dfec430
kckc0608 <kckc0608@gmail.com>
2025-02-16 11:57:16 +0900

    feat: 사회복지사 비밀번호 검증 로직 추가

--- src/main/java/com/becareful/becarefulserver/domain/socialworker/repository/SocialworkerRepository.java
+++ src/main/java/com/becareful/becarefulserver/domain/socialworker/repository/SocialworkerRepository.java
@@ -3,5 +3,9 @@ package com.becareful.becarefulserver.domain.socialworker.repository;
 import com.becareful.becarefulserver.domain.socialworker.domain.Socialworker;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SocialworkerRepository extends JpaRepository<Socialworker, Long> {
+
+    Optional<Socialworker> findByPhoneNumber(String phoneNumber);
 }
