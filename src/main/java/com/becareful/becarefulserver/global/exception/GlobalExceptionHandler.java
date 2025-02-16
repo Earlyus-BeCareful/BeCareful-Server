501bb4ed05efd9c3c98c82222c8605e9412d9937
kckc0608 <kckc0608@gmail.com>
2025-02-16 10:34:28 +0900

    feat: GlobalExceptionHandler 추가

---
+++ src/main/java/com/becareful/becarefulserver/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,42 @@
+package com.becareful.becarefulserver.global.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.becareful.becarefulserver.global.exception.exception.AuthException;
+import com.becareful.becarefulserver.global.exception.exception.DomainException;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DomainException.class)
+    public ResponseEntity<ErrorResponse> domainException(DomainException e) {
+        log.info(e.getMessage(), e);
+        return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(AuthException.class)
+    public ResponseEntity<ErrorResponse> authException(final AuthException e) {
+        log.info("AuthException: {}", e.getMessage(), e);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(final MethodArgumentNotValidException ex) {
+        log.info("MethodArgumentNotValidException: {}", ex.getMessage(), ex);
+        return ResponseEntity.badRequest()
+                .body(new ErrorResponse(ex.getBindingResult().getAllErrors().get(0).getDefaultMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleUnknownException(Exception e) {
+        log.error("Unknown exception: {}", e.getMessage(), e);
+        return ResponseEntity.internalServerError().body(new ErrorResponse(e.getMessage()));
+    }
+}
