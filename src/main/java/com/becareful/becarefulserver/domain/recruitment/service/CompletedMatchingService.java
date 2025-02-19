de30f975a000713292cedecc1123215c20155abf
Everdu <kckc0608@gmail.com>
2025-02-20 05:33:51 +0900

    feat: 계약서 최종 승인 API (#90)
    * feat: 매칭 현황 리스트 조회 API 검수
    
    * chore: 디테일한 부분 수정
    
    * fix: 매칭 상세 조회에서 요양보호사 id 추가
    
    * feat: 요양보호사 상세 정보 조회 api
    
    * feat: 채용하기 버튼 API
    
    * feat: 게약서 최종 승인 API

--- src/main/java/com/becareful/becarefulserver/domain/recruitment/service/CompletedMatchingService.java
+++ src/main/java/com/becareful/becarefulserver/domain/recruitment/service/CompletedMatchingService.java
@@ -2,28 +2,34 @@ package com.becareful.becarefulserver.domain.recruitment.service;
 
 import com.becareful.becarefulserver.domain.caregiver.domain.Caregiver;
 import com.becareful.becarefulserver.domain.recruitment.domain.CompletedMatching;
+import com.becareful.becarefulserver.domain.recruitment.domain.Contract;
 import com.becareful.becarefulserver.domain.recruitment.dto.request.EditCompletedMatchingNoteRequest;
 import com.becareful.becarefulserver.domain.recruitment.dto.response.CompletedMatchingInfoResponse;
 import com.becareful.becarefulserver.domain.recruitment.repository.CompletedMatchingRepository;
+import com.becareful.becarefulserver.domain.recruitment.repository.ContractRepository;
 import com.becareful.becarefulserver.global.util.AuthUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class CompletedMatchingService {
+
     private final CompletedMatchingRepository completedMatchingRepository;
     private final AuthUtil authUtil;
+    private final ContractRepository contractRepository;
 
     public List<CompletedMatchingInfoResponse> getCompletedMatchings() {
         Caregiver caregiver = authUtil.getLoggedInCaregiver();
         List<CompletedMatching> completedMatchings = completedMatchingRepository.findByCaregiver(caregiver);
         return completedMatchings.stream()
-                .map(matching -> CompletedMatchingInfoResponse.form(matching, matching.getElderly()))
-                .collect(Collectors.toList());
+                .map(CompletedMatchingInfoResponse::from)
+                .toList();
     }
 
     public void editNote(Long completedMatchingId, EditCompletedMatchingNoteRequest request){
@@ -31,5 +37,14 @@ public class CompletedMatchingService {
                 .orElseThrow(() -> new IllegalArgumentException("Matching not found"));
 
         completedMatching.updateNote(request.note());
+    }
+
+    @Transactional
+    public void createCompletedMatching(Long contractId) {
+        Caregiver loggedInCaregiver = authUtil.getLoggedInCaregiver();
+        Contract contract = contractRepository.findById(contractId).orElseThrow(() -> new IllegalArgumentException("Contract not found"));
+
+        CompletedMatching completedMatching = new CompletedMatching(loggedInCaregiver, contract);
+        completedMatchingRepository.save(completedMatching);
     }
 }
