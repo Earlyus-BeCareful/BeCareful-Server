f065e920fcf69fafbc27774b5348a91b19e31a9d
RyuJiye <coomicuma@naver.com>
2025-02-19 19:10:44 +0900

    어르신 관련 API 수정

--- src/main/java/com/becareful/becarefulserver/domain/socialworker/controller/ElderlyController.java
+++ src/main/java/com/becareful/becarefulserver/domain/socialworker/controller/ElderlyController.java
@@ -1,6 +1,5 @@
 package com.becareful.becarefulserver.domain.socialworker.controller;
 
-import com.becareful.becarefulserver.domain.caregiver.dto.response.CaregiverProfileUploadResponse;
 import com.becareful.becarefulserver.domain.socialworker.dto.request.ElderlyCreateRequest;
 import com.becareful.becarefulserver.domain.socialworker.dto.request.ElderlyUpdateRequest;
 import com.becareful.becarefulserver.domain.socialworker.dto.response.ElderlyProfileUploadResponse;
@@ -16,21 +15,21 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.net.URI;
 
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/elderly")
 @Tag(name = "Elderly", description = "어르신 관련 API 입니다.") //상세 주소 포함하기
 public class ElderlyController {
     private final ElderlyService elderlyService;
-
-    @Operation(summary = "어르신 등록", description = "요양 기관 ID 필수")
+    @Operation(summary = "어르신 등록")
     @PostMapping("/register")
     public ResponseEntity<Void> createElderly(@Valid @RequestBody ElderlyCreateRequest request){
         Long id = elderlyService.saveElderly(request);
         return ResponseEntity.created(URI.create("/elderly/" + id)).build();
     }
     //어르신 수정
-    @Operation(summary = "어르신 정보 수정", description = "어르신 정보를 수정하는 API 입니다. 프로필이미지를 기본 이미지로 업데이트 시 반드시 공백 urI을 넣어주세요.")
+    @Operation(summary = "어르신 정보 수정", description = "어르신 정보를 수정하는 API 입니다. 프로필이미지를 기본 이미지로 업데이트 시 반드시 공백 uri을 넣어주세요.")
     @PatchMapping("/update/{elderlyId}")
     public ResponseEntity<Void> updateElderly(
             @PathVariable Long elderlyId,
@@ -43,12 +42,12 @@ public class ElderlyController {
 
     //어르신 상세 - 생년월일, 나이, 거주지 상세 주소, 동거인, 동물, 건강상태, 요양등급, 케어필요항목, 담당 보호사(프로필, 성함, 요일, 시간, 케어항목), 모집중인 공고(요일, 시간, 케어항목)
 
-    @Operation(summary = "어르신 프로필 사진 신규 업로드", description = "어르신 등록시 사용하는 프로필 이미지 업로드 API 입니다.")
+    @Operation(summary = "어르신 프로필 사진 업로드", description = "어르신 등록, 수정 시 사용하는 프로필 이미지 업로드 API 입니다.")
     @PostMapping(value = "/upload-profile-img", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<ElderlyProfileUploadResponse> uploadProfileImg(
             @RequestPart MultipartFile file,
-            @RequestPart String phoneNumber) {
-        var response = elderlyService.uploadProfileImage(file, phoneNumber);
+            @RequestPart String institutionId) {
+        var response = elderlyService.uploadProfileImage(file, institutionId);
         return ResponseEntity.ok(response);
     }
 }
