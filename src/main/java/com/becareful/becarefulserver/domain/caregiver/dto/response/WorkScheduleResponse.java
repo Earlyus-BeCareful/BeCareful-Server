f959feab85e2a5960c3769430012579bac2fbc17
kckc0608 <kckc0608@gmail.com>
2025-02-20 06:09:28 +0900

    feat: 요양보호사 홈화면 실제 데이터

--- src/main/java/com/becareful/becarefulserver/domain/caregiver/dto/response/WorkScheduleResponse.java
+++ src/main/java/com/becareful/becarefulserver/domain/caregiver/dto/response/WorkScheduleResponse.java
@@ -1,7 +1,10 @@
 package com.becareful.becarefulserver.domain.caregiver.dto.response;
 
+import static java.time.format.DateTimeFormatter.ofPattern;
+
 import com.becareful.becarefulserver.domain.common.domain.CareType;
 import com.becareful.becarefulserver.domain.common.vo.Gender;
+import com.becareful.becarefulserver.domain.recruitment.domain.CompletedMatching;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalTime;
@@ -17,20 +20,30 @@ public record WorkScheduleResponse(
         String seniorName,
         Gender seniorGender,
         Integer seniorAge,
-        @Schema(description = "더미 형식이므로 변경될 수 있습니다.")
         List<CareType> seniorCareType,
-        @Schema(description = "더미 형식이므로 변경될 수 있습니다.")
         String workLocation
 ) {
     public static WorkScheduleResponse createDummy() {
         return WorkScheduleResponse.builder()
-                .workStartTime(LocalTime.of(8, 20).format(DateTimeFormatter.ofPattern("HH:mm")))
-                .workEndTime(LocalTime.of(17, 20).format(DateTimeFormatter.ofPattern("HH:mm")))
+                .workStartTime(LocalTime.of(8, 20).format(ofPattern("HH:mm")))
+                .workEndTime(LocalTime.of(17, 20).format(ofPattern("HH:mm")))
                 .seniorName("박순자")
                 .seniorGender(Gender.FEMALE)
                 .seniorAge(65)
                 .seniorCareType(List.of(CareType.식사보조, CareType.이동보조))
                 .workLocation("노원구 상계동")
+                .build();
+    }
+
+    public static WorkScheduleResponse from(CompletedMatching completedMatching) {
+        return WorkScheduleResponse.builder()
+                .workStartTime(completedMatching.getContract().getWorkStartTime().format(ofPattern("HH:mm")))
+                .workEndTime(completedMatching.getContract().getWorkEndTime().format(ofPattern("HH:mm")))
+                .seniorName(completedMatching.getContract().getMatching().getRecruitment().getElderly().getName())
+                .seniorGender(completedMatching.getContract().getMatching().getRecruitment().getElderly().getGender())
+                .seniorAge(completedMatching.getContract().getMatching().getRecruitment().getElderly().getAge())
+                .seniorCareType(completedMatching.getContract().getCareTypes().stream().toList())
+                .workLocation(completedMatching.getContract().getMatching().getRecruitment().getElderly().getResidentialAddress().getFullAddress())
                 .build();
     }
 }
