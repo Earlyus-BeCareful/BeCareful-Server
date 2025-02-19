6fbf48a892e63814104e9702dfe89ec217dcc882
RyuJiye <coomicuma@naver.com>
2025-02-20 07:43:22 +0900

    계약서 API 수정

--- src/main/java/com/becareful/becarefulserver/domain/recruitment/controller/ContractController.java
+++ src/main/java/com/becareful/becarefulserver/domain/recruitment/controller/ContractController.java
@@ -22,7 +22,7 @@ public class ContractController {
 
     //TODO
     @Operation(summary = "계약서(근무조건) 리스트", description = "채팅화면에 계약서 반환")
-    @GetMapping("/contract/list/{matchingId}")
+    @GetMapping("/list/{matchingId}")
 
     public ResponseEntity<ContractInfoResponseList> getContractListAndInfo(@PathVariable Long matchingId){
         var response = contractService.getContractListAndInfo(matchingId);
@@ -32,7 +32,7 @@ public class ContractController {
     //TODO
     //계약서 상세 정보 불러오기(수정할때 사용)
     @Operation(summary = "근무조건 상세 내용 반환", description = "근무조건 수정시 이전 조건 내용을 불러오는데 사용하는 API.")
-    @GetMapping("/contract/{contractId}/detail")
+    @GetMapping("/{contractId}/detail")
     public ResponseEntity<ContractDetailResponse> getContractDetail(@PathVariable Long contractId){
         var response = contractService.getContractDetail(contractId);
         return ResponseEntity.ok(response);
@@ -41,7 +41,7 @@ public class ContractController {
     //TODO
     //계약서 수정 내용 저장 - 직전 계약서 필요
     @Operation(summary = "근무조건 수정 내용 저장")
-    @PostMapping("/contract/edit")
+    @PostMapping("/edit")
     public ResponseEntity<Void> editContract(@RequestBody ContractEditRequest request){
         contractService.editContract(request);
         return ResponseEntity.ok().build();
