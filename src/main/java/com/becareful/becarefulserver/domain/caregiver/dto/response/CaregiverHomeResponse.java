a5df1ee2b11849818a9bd1bc3e34e4f2207265a2
Everdu <kckc0608@gmail.com>
2025-02-20 06:11:22 +0900

    feat: 요양보호사 홈화면 실제 데이터 (#91)
    * feat: 매칭 현황 리스트 조회 API 검수
    
    * chore: 디테일한 부분 수정
    
    * fix: 매칭 상세 조회에서 요양보호사 id 추가
    
    * feat: 요양보호사 상세 정보 조회 api
    
    * feat: 채용하기 버튼 API
    
    * feat: 게약서 최종 승인 API
    
    * feat: 요양보호사 홈화면 실제 데이터

--- src/main/java/com/becareful/becarefulserver/domain/caregiver/dto/response/CaregiverHomeResponse.java
+++ src/main/java/com/becareful/becarefulserver/domain/caregiver/dto/response/CaregiverHomeResponse.java
@@ -12,19 +12,16 @@ public record CaregiverHomeResponse(
         String name,
         Integer applicationCount,
         Integer recruitmentCount,
-        @Schema(description = "더미 데이터이므로 변경될 수 있습니다.")
         List<WorkScheduleResponse> workScheduleList,
         boolean isWorking
 ) {
-    public static CaregiverHomeResponse of(Caregiver caregiver) {
+    public static CaregiverHomeResponse of(Caregiver caregiver, Integer applicationCount, Integer recruitmentCount, boolean isWorking, List<WorkScheduleResponse> workScheduleList) {
         return CaregiverHomeResponse.builder()
                 .name(caregiver.getName())
-                .applicationCount(0)
-                .recruitmentCount(0)
-                .workScheduleList(List.of(
-                        WorkScheduleResponse.createDummy(),
-                        WorkScheduleResponse.createDummy()))
-                .isWorking(false) // TODO : 더미 데이터에서 실제 데이터로 변경 필요
+                .applicationCount(applicationCount)
+                .recruitmentCount(recruitmentCount)
+                .workScheduleList(workScheduleList)
+                .isWorking(isWorking)
                 .build();
     }
 }
