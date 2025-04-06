8840e56a82ba103435f74ef90b2cebc0e8c010c6
kckc0608 <kckc0608@naver.com>
2025-04-06 20:10:34 +0900

    refator: request mapping 묶기

--- src/main/java/com/becareful/becarefulserver/domain/caregiver/controller/WorkApplicationController.java
+++ src/main/java/com/becareful/becarefulserver/domain/caregiver/controller/WorkApplicationController.java
@@ -12,34 +12,35 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@Tag(name = "WorkApplication", description = "요양보호사 일자리 신청 관련 API 입니다.")
+@RequestMapping("/caregiver/work-application")
+@Tag(name = "Work Application", description = "요양보호사 일자리 신청 관련 API 입니다.")
 public class WorkApplicationController {
 
     private final WorkApplicationService workApplicationService;
 
     @Operation(summary = "일자리 신청 정보 등록/수정")
-    @PutMapping("/work-application")
+    @PutMapping
     public ResponseEntity<Void> updateWorkApplication(@Valid @RequestBody WorkApplicationUpdateRequest request) {
         workApplicationService.updateWorkApplication(request);
         return ResponseEntity.ok().build();
     }
 
     @Operation(summary = "일자리 신청 정보 조회", description = "신청 정보를 등록한 적이 없다면 빈 값을 반환합니다.")
-    @GetMapping("/work-application")
+    @GetMapping
     public ResponseEntity<WorkApplicationResponse> getWorkApplication() {
         WorkApplicationResponse response = workApplicationService.getWorkApplication();
         return ResponseEntity.ok(response);
     }
 
     @Operation(summary = "일자리 신청 활성화", description = "등록한 일자리 신청 내용을 기반으로 매칭을 활성화 합니다.")
-    @PostMapping("/work-applicatioin/active")
+    @PostMapping("/active")
     public ResponseEntity<Void> updateWorkApplicationActive() {
         workApplicationService.updateWorkApplicationActive();
         return ResponseEntity.ok().build();
     }
 
     @Operation(summary = "일자리 신청 비활성화", description = "일자리 매칭을 비활성화 합니다.")
-    @PostMapping("/work-applicatioin/inactive")
+    @PostMapping("/inactive")
     public ResponseEntity<Void> updateWorkApplicationInactive() {
         workApplicationService.updateWorkApplicationInactive();
         return ResponseEntity.ok().build();
