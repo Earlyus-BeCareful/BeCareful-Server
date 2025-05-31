b9b093189d7c9a62162ad652393a10fdbf009e82
kckc0608 <kckc0608@naver.com>
2025-05-31 23:11:49 +0900

    feat: board type, 협회 연관 관계 추가

---
+++ src/main/java/com/becareful/becarefulserver/domain/community/domain/BoardType.java
@@ -0,0 +1,13 @@
+package com.becareful.becarefulserver.domain.community.domain;
+
+public enum BoardType {
+    /**
+     * ASSOCIATION = 협회 (각 기관장이 속한 협회)
+     * INSTITUTION = 기관 (사회복지사가 소속된 기관)
+     * SERVICE = 공단 (건강보험공단)
+     **/
+    ASSOCIATION_NOTICE,
+    SERVICE_NOTICE,
+    INFORMATION_SHARING,
+    PARTICIPATION_APPLICATION
+}
