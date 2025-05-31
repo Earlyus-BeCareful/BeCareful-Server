57783d51c1bc6d94f79f117ee0ca71a849e10599
kckc0608 <kckc0608@naver.com>
2025-06-01 00:08:26 +0900

    fix: enum 에 string 적용

--- src/main/java/com/becareful/becarefulserver/domain/community/domain/PostBoard.java
+++ src/main/java/com/becareful/becarefulserver/domain/community/domain/PostBoard.java
@@ -22,6 +22,7 @@ public class PostBoard extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Enumerated(EnumType.STRING)
     private BoardType boardType;
 
     @Enumerated(EnumType.STRING)
