cf823a1a77bc38fb9337e3cad6338b201a5aec98
RyuJiye <coomicuma@naver.com>
2025-06-01 21:32:59 +0900

    style: fix formatting issues

--- src/main/java/com/becareful/becarefulserver/domain/community/service/CommunityService.java
+++ src/main/java/com/becareful/becarefulserver/domain/community/service/CommunityService.java
@@ -12,15 +12,14 @@ import com.becareful.becarefulserver.global.util.AuthUtil;
 import com.becareful.becarefulserver.global.util.JwtUtil;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
