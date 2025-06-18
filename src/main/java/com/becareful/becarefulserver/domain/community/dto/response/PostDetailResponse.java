054f92dc9020f3e79d5fb64faa0670f720469e1e
kckc0608 <kckc0608@naver.com>
2025-06-19 01:16:31 +0900

    feat: 게시글 조회시 이미지, 비디오 url 반환 하도록 추가

--- src/main/java/com/becareful/becarefulserver/domain/community/dto/response/PostDetailResponse.java
+++ src/main/java/com/becareful/becarefulserver/domain/community/dto/response/PostDetailResponse.java
@@ -1,8 +1,8 @@
 package com.becareful.becarefulserver.domain.community.dto.response;
 
 import com.becareful.becarefulserver.domain.community.domain.Post;
+import com.becareful.becarefulserver.domain.community.domain.PostMedia;
 import com.becareful.becarefulserver.domain.community.dto.AuthorSimpleDto;
-import com.becareful.becarefulserver.domain.community.dto.PostMediaDto;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
@@ -14,8 +14,8 @@ public record PostDetailResponse(
         boolean isEdited,
         String postedDate,
         AuthorSimpleDto author,
-        List<PostMediaDto> images,
-        List<PostMediaDto> videos) {
+        List<String> imageUrls,
+        List<String> videoUrls) {
     public static PostDetailResponse from(Post post) {
         return new PostDetailResponse(
                 post.getId(),
@@ -25,7 +25,7 @@ public record PostDetailResponse(
                 !post.getCreateDate().isEqual(post.getUpdateDate()),
                 post.getUpdateDate().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME),
                 AuthorSimpleDto.from(post.getAuthor()),
-                post.getImageMediaList().stream().map(PostMediaDto::from).toList(),
-                post.getVideoMediaList().stream().map(PostMediaDto::from).toList());
+                post.getImageMediaList().stream().map(PostMedia::getMediaUrl).toList(),
+                post.getVideoMediaList().stream().map(PostMedia::getMediaUrl).toList());
     }
 }
