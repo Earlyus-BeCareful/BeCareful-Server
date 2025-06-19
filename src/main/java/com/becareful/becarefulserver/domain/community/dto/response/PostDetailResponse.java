42553ab607d6ab5620956290f530a9ccdbc00f78
kckc0608 <kckc0608@naver.com>
2025-06-19 23:54:10 +0900

    feat: 게시글 상세 조회시 나의 글 여부 필드 추가

--- src/main/java/com/becareful/becarefulserver/domain/community/dto/response/PostDetailResponse.java
+++ src/main/java/com/becareful/becarefulserver/domain/community/dto/response/PostDetailResponse.java
@@ -4,6 +4,7 @@ import com.becareful.becarefulserver.domain.community.domain.FileType;
 import com.becareful.becarefulserver.domain.community.domain.Post;
 import com.becareful.becarefulserver.domain.community.domain.PostMedia;
 import com.becareful.becarefulserver.domain.community.dto.AuthorSimpleDto;
+import com.becareful.becarefulserver.domain.socialworker.domain.SocialWorker;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
@@ -17,7 +18,8 @@ public record PostDetailResponse(
         AuthorSimpleDto author,
         List<String> imageUrls,
         List<String> videoUrls,
-        List<String> fileUrls) {
+        List<String> fileUrls,
+        boolean isMyPost) {
     public static PostDetailResponse from(Post post) {
         return new PostDetailResponse(
                 post.getId(),
@@ -35,6 +37,31 @@ public record PostDetailResponse(
                         .toList(),
                 post.getMediaListByType(FileType.FILE).stream()
                         .map(PostMedia::getMediaUrl)
-                        .toList());
+                        .toList(),
+                false // 기본값: 내 글 아님
+                );
+    }
+
+    public static PostDetailResponse from(Post post, Long currentUserId) {
+        SocialWorker author = post.getAuthor();
+        boolean isMyPost = author != null && author.getId().equals(currentUserId);
+        return new PostDetailResponse(
+                post.getId(),
+                post.getTitle(),
+                post.getContent(),
+                post.isImportant(),
+                !post.getCreateDate().isEqual(post.getUpdateDate()),
+                post.getUpdateDate().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME),
+                AuthorSimpleDto.from(author),
+                post.getMediaListByType(FileType.IMAGE).stream()
+                        .map(PostMedia::getMediaUrl)
+                        .toList(),
+                post.getMediaListByType(FileType.VIDEO).stream()
+                        .map(PostMedia::getMediaUrl)
+                        .toList(),
+                post.getMediaListByType(FileType.FILE).stream()
+                        .map(PostMedia::getMediaUrl)
+                        .toList(),
+                isMyPost);
     }
 }
