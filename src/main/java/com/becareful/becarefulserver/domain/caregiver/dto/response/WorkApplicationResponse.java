522632478e9ce9dfee923364bfc03bf5d7f5e9e4
Everdu <kckc0608@gmail.com>
2025-07-02 00:41:50 +0900

    fix: '일급' 타입 추가 (#198)
    * fix: 일급 타입 추가
    
    * refactor: 월급 단위 타입 클래스 명 변경
    
    * chore: spotless 적용
    
    * fix: 변경된 변수명에 따라 builder 코드 수정

--- src/main/java/com/becareful/becarefulserver/domain/caregiver/dto/response/WorkApplicationResponse.java
+++ src/main/java/com/becareful/becarefulserver/domain/caregiver/dto/response/WorkApplicationResponse.java
@@ -1,7 +1,7 @@
 package com.becareful.becarefulserver.domain.caregiver.dto.response;
 
 import com.becareful.becarefulserver.domain.caregiver.domain.WorkApplication;
-import com.becareful.becarefulserver.domain.caregiver.domain.WorkSalaryType;
+import com.becareful.becarefulserver.domain.caregiver.domain.WorkSalaryUnitType;
 import com.becareful.becarefulserver.domain.caregiver.domain.WorkTime;
 import com.becareful.becarefulserver.domain.common.domain.CareType;
 import com.becareful.becarefulserver.domain.work_location.dto.request.WorkLocationDto;
@@ -13,7 +13,7 @@ public record WorkApplicationResponse(
         List<DayOfWeek> workDays,
         List<WorkTime> workTimes,
         List<CareType> careTypes,
-        WorkSalaryType workSalaryType,
+        WorkSalaryUnitType workSalaryUnitType,
         Integer workSalaryAmount) {
     public static WorkApplicationResponse of(List<WorkLocationDto> locations, WorkApplication application) {
         return new WorkApplicationResponse(
@@ -21,7 +21,7 @@ public record WorkApplicationResponse(
                 application.getWorkDays().stream().toList(),
                 application.getWorkTimes().stream().toList(),
                 application.getWorkCareTypes().stream().toList(),
-                application.getWorkSalaryType(),
+                application.getWorkSalaryUnitType(),
                 application.getWorkSalaryAmount());
     }
 
