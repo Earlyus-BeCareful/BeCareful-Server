44210c985e0d78741ed76145e0078d18d044b187
kckc0608 <kckc0608@naver.com>
2025-07-02 01:12:01 +0900

    feat: 요양보호사 매칭 적합도 데이터 추가

--- src/main/java/com/becareful/becarefulserver/domain/matching/dto/response/MatchingStatusDetailResponse.java
+++ src/main/java/com/becareful/becarefulserver/domain/matching/dto/response/MatchingStatusDetailResponse.java
@@ -1,19 +1,18 @@
 package com.becareful.becarefulserver.domain.matching.dto.response;
 
 import com.becareful.becarefulserver.domain.matching.domain.Recruitment;
-import com.becareful.becarefulserver.domain.matching.dto.CaregiverSimpleDto;
 import com.becareful.becarefulserver.domain.matching.dto.RecruitmentSimpleDto;
 import java.util.List;
 
 public record MatchingStatusDetailResponse(
         RecruitmentSimpleDto recruitmentInfo,
-        List<CaregiverSimpleDto> matchedCaregivers,
-        List<CaregiverSimpleDto> appliedCaregivers) {
+        List<MatchingCaregiverSimpleResponse> matchedCaregivers,
+        List<MatchingCaregiverSimpleResponse> appliedCaregivers) {
 
     public static MatchingStatusDetailResponse of(
             Recruitment recruitment,
-            List<CaregiverSimpleDto> matchedCaregivers,
-            List<CaregiverSimpleDto> appliedCaregivers) {
+            List<MatchingCaregiverSimpleResponse> matchedCaregivers,
+            List<MatchingCaregiverSimpleResponse> appliedCaregivers) {
         return new MatchingStatusDetailResponse(
                 RecruitmentSimpleDto.from(recruitment), matchedCaregivers, appliedCaregivers);
     }
