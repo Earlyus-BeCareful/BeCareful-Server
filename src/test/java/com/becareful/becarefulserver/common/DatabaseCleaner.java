06dab7a6787a68a576578e1cae86151d013b01bd
Everdu <kckc0608@gmail.com>
2025-07-21 16:52:51 +0900

    Fix DatabaseCleaner for snake_case tables

--- src/test/java/com/becareful/becarefulserver/common/DatabaseCleaner.java
+++ src/test/java/com/becareful/becarefulserver/common/DatabaseCleaner.java
@@ -1,7 +1,9 @@
 package com.becareful.becarefulserver.common;
 
+import com.becareful.becarefulserver.domain.association.repository.AssociationRepository;
 import com.becareful.becarefulserver.domain.nursing_institution.repository.NursingInstitutionRepository;
 import com.becareful.becarefulserver.domain.socialworker.repository.SocialWorkerRepository;
+import com.becareful.becarefulserver.fixture.AssociationFixture;
 import com.becareful.becarefulserver.fixture.NursingInstitutionFixture;
 import com.becareful.becarefulserver.fixture.SocialWorkerFixture;
 import jakarta.persistence.EntityManager;
@@ -16,8 +18,9 @@ public class DatabaseCleaner {
     @PersistenceContext
     private EntityManager em;
 
-    @Autowired
-    private SocialWorkerRepository socialworkerRepository;
+    @Autowired private SocialWorkerRepository socialworkerRepository;
+
+    @Autowired private AssociationRepository associationRepository;
 
     @Autowired
     private NursingInstitutionRepository institutionRepository;
@@ -25,12 +28,20 @@ public class DatabaseCleaner {
     @Transactional
     public void clean() {
         em.createNativeQuery("set foreign_key_checks = 0").executeUpdate();
-        em.getMetamodel().getEntities().forEach(entity -> {
-            em.createNativeQuery("truncate table " + entity.getName()).executeUpdate();
-        });
+        em.getMetamodel().getEntities()
+                .forEach(
+                        entity -> {
+                            String tableName = camelToSnake(entity.getName());
+                            em.createNativeQuery("truncate table " + tableName).executeUpdate();
+                        });
         em.createNativeQuery("set foreign_key_checks = 1").executeUpdate();
         institutionRepository.save(NursingInstitutionFixture.NURSING_INSTITUTION);
+        associationRepository.save(AssociationFixture.JEONJU_ASSOCIATION);
         socialworkerRepository.save(SocialWorkerFixture.SOCIAL_WORKER_1);
         socialworkerRepository.save(SocialWorkerFixture.SOCIAL_WORKER_MANAGER);
+    }
+
+    private String camelToSnake(String value) {
+        return value.replaceAll("(?<=[a-z])[A-Z]", "_$0").toLowerCase();
     }
 }
