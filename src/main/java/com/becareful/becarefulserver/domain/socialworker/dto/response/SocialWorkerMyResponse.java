ee0a5288298d65245d11a7728e9a7d83ce31dda9
kckc0608 <kckc0608@naver.com>
2025-07-22 23:51:49 +0900

    refactor: social worker my page response

--- src/main/java/com/becareful/becarefulserver/domain/socialworker/dto/response/SocialWorkerMyResponse.java
+++ src/main/java/com/becareful/becarefulserver/domain/socialworker/dto/response/SocialWorkerMyResponse.java
@@ -3,7 +3,6 @@ package com.becareful.becarefulserver.domain.socialworker.dto.response;
 import com.becareful.becarefulserver.domain.association.domain.Association;
 import com.becareful.becarefulserver.domain.nursing_institution.domain.NursingInstitution;
 import com.becareful.becarefulserver.domain.socialworker.domain.SocialWorker;
-import com.becareful.becarefulserver.domain.socialworker.domain.vo.AssociationRank;
 import com.becareful.becarefulserver.domain.socialworker.dto.InstitutionDto;
 import com.becareful.becarefulserver.domain.socialworker.dto.SocialWorkerSimpleDto;
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -11,17 +10,13 @@ import jakarta.annotation.Nullable;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record SocialWorkerMyResponse(
-        SocialWorkerSimpleDto socialWorkerInfo,
-        InstitutionDto institutionInfo,
-        @Nullable String associationName,
-        @Nullable AssociationRank associationRank) {
+        SocialWorkerSimpleDto socialWorkerInfo, InstitutionDto institutionInfo, @Nullable String associationName) {
     public static SocialWorkerMyResponse from(SocialWorker socialWorker) {
         NursingInstitution institution = socialWorker.getNursingInstitution();
         Association association = socialWorker.getAssociation();
         return new SocialWorkerMyResponse(
                 SocialWorkerSimpleDto.from(socialWorker),
                 InstitutionDto.from(institution),
-                association != null ? association.getName() : null,
-                association != null ? socialWorker.getAssociationRank() : null);
+                association != null ? association.getName() : null);
     }
 }
