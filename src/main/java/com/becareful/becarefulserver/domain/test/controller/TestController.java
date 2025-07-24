91005020c258c5043ca5893ea33c82435c7e5022
kckc0608 <kckc0608@naver.com>
2025-07-25 00:56:22 +0900

    chore: spotless 적용

--- src/main/java/com/becareful/becarefulserver/domain/test/controller/TestController.java
+++ src/main/java/com/becareful/becarefulserver/domain/test/controller/TestController.java
@@ -1,10 +1,12 @@
 package com.becareful.becarefulserver.domain.test.controller;
 
+import static com.becareful.becarefulserver.global.exception.ErrorMessage.CAREGIVER_NOT_EXISTS;
+import static com.becareful.becarefulserver.global.exception.ErrorMessage.SOCIAL_WORKER_NOT_EXISTS;
+
 import com.becareful.becarefulserver.domain.caregiver.domain.Caregiver;
 import com.becareful.becarefulserver.domain.caregiver.repository.CaregiverRepository;
 import com.becareful.becarefulserver.domain.socialworker.domain.SocialWorker;
 import com.becareful.becarefulserver.domain.socialworker.repository.SocialWorkerRepository;
-import com.becareful.becarefulserver.global.exception.ErrorMessage;
 import com.becareful.becarefulserver.global.exception.exception.CaregiverException;
 import com.becareful.becarefulserver.global.exception.exception.SocialWorkerException;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -14,9 +16,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import static com.becareful.becarefulserver.global.exception.ErrorMessage.CAREGIVER_NOT_EXISTS;
-import static com.becareful.becarefulserver.global.exception.ErrorMessage.SOCIAL_WORKER_NOT_EXISTS;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,7 +28,8 @@ public class TestController {
 
     @DeleteMapping("/social-worker")
     public ResponseEntity<Void> deleteSocialWorker(@RequestParam String phoneNumber) {
-        SocialWorker socialWorker = socialWorkerRepository.findByPhoneNumber(phoneNumber)
+        SocialWorker socialWorker = socialWorkerRepository
+                .findByPhoneNumber(phoneNumber)
                 .orElseThrow(() -> new SocialWorkerException(SOCIAL_WORKER_NOT_EXISTS));
         socialWorkerRepository.delete(socialWorker);
         return ResponseEntity.noContent().build();
@@ -37,7 +37,8 @@ public class TestController {
 
     @DeleteMapping("/caregiver")
     public ResponseEntity<Void> deleteCaregiver(@RequestParam String phoneNumber) {
-        Caregiver caregiver = caregiverRepository.findByPhoneNumber(phoneNumber)
+        Caregiver caregiver = caregiverRepository
+                .findByPhoneNumber(phoneNumber)
                 .orElseThrow(() -> new CaregiverException(CAREGIVER_NOT_EXISTS));
         caregiverRepository.delete(caregiver);
         return ResponseEntity.noContent().build();
