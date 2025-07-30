fc4deb3d0f724bb216f21cc78b9ffe6530b62fca
kckc0608 <kckc0608@naver.com>
2025-07-30 18:03:48 +0900

    feat: 채팅 데이터 조회시 어르신 이미지 정보 추가

--- src/main/java/com/becareful/becarefulserver/domain/chat/dto/response/ChatroomContentResponse.java
+++ src/main/java/com/becareful/becarefulserver/domain/chat/dto/response/ChatroomContentResponse.java
@@ -3,17 +3,19 @@ package com.becareful.becarefulserver.domain.chat.dto.response;
 import com.becareful.becarefulserver.domain.chat.dto.ContractDto;
 import com.becareful.becarefulserver.domain.matching.domain.Contract;
 import com.becareful.becarefulserver.domain.matching.domain.Matching;
+import com.becareful.becarefulserver.domain.matching.dto.ElderlySimpleDto;
+
 import java.util.List;
 
 public record ChatroomContentResponse(
-        String elderlyName,
+        ElderlySimpleDto elderlyInfo,
         String nursingInstitutionName,
         String careGiverName,
         List<ContractDto> contractInfoResponseList) {
 
     public static ChatroomContentResponse of(Matching matching, List<Contract> contractList) {
         return new ChatroomContentResponse(
-                matching.getRecruitment().getElderly().getName(),
+                ElderlySimpleDto.from(matching.getRecruitment().getElderly()),
                 matching.getRecruitment().getElderly().getNursingInstitution().getName(),
                 matching.getWorkApplication().getCaregiver().getName(),
                 contractList.stream().map(ContractDto::from).toList());
