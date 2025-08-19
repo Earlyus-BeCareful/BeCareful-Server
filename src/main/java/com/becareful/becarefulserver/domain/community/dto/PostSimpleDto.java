ba36f12b9966d0b34522c6745dfdcea527004a8f
Everdu <kckc0608@gmail.com>
2025-08-20 00:06:20 +0900

    feat: 썸네일 url 응답하도록 추가 (#304)

--- src/main/java/com/becareful/becarefulserver/domain/community/dto/PostSimpleDto.java
+++ src/main/java/com/becareful/becarefulserver/domain/community/dto/PostSimpleDto.java
@@ -1,8 +1,12 @@
 package com.becareful.becarefulserver.domain.community.dto;
 
+import static com.becareful.becarefulserver.domain.community.domain.FileType.IMAGE;
+
 import com.becareful.becarefulserver.domain.community.domain.BoardType;
 import com.becareful.becarefulserver.domain.community.domain.Post;
+import com.becareful.becarefulserver.domain.community.domain.PostMedia;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 public record PostSimpleDto(
         Long postId,
@@ -13,12 +17,13 @@ public record PostSimpleDto(
         String createdAt,
         AuthorSimpleDto author) {
     public static PostSimpleDto from(Post post) {
+        List<PostMedia> images = post.getMediaListByType(IMAGE);
         return new PostSimpleDto(
                 post.getId(),
                 post.getTitle(),
                 post.getBoard().getBoardType(),
                 post.isImportant(),
-                "", // TODO : 이미지 관련 연관관계 설정 후 작업
+                images.isEmpty() ? null : images.get(0).getMediaUrl(),
                 post.getCreateDate().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME),
                 AuthorSimpleDto.from(post.getAuthor()));
     }
