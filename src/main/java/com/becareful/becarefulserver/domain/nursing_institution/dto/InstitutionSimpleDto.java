beebc8046db7a9837d9a2fcf372c364091fb1753
Everdu <kckc0608@gmail.com>
2025-09-08 20:58:26 +0900

    refactor: 기관 리스트 응답에 기관 코드 추가 (#338)

--- src/main/java/com/becareful/becarefulserver/domain/nursing_institution/dto/InstitutionSimpleDto.java
+++ src/main/java/com/becareful/becarefulserver/domain/nursing_institution/dto/InstitutionSimpleDto.java
@@ -2,10 +2,11 @@ package com.becareful.becarefulserver.domain.nursing_institution.dto;
 
 import com.becareful.becarefulserver.domain.nursing_institution.domain.NursingInstitution;
 
-public record InstitutionSimpleDto(Long institutionId, String name, String address) {
+public record InstitutionSimpleDto(Long institutionId, String institutionCode, String name, String address) {
     public static InstitutionSimpleDto from(NursingInstitution institution) {
         return new InstitutionSimpleDto(
                 institution.getId(),
+                institution.getCode(),
                 institution.getName(),
                 institution.getAddress().getFullAddress());
     }
