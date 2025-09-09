d39f7b1e8212e0e7e7044bf2e7925367d24a50e4
Jiye Ryu <ryujy0716@gmail.com>
2025-09-09 11:42:04 +0900

    Feature/288 caregiver logout and leave (#351)
    * feat: 요양보호사 로그아웃
    
    * feat:요양보호사 탈퇴 구현
    
    * spotless 적용, 테스트 수정
    
    * spotless 적용

--- src/main/java/com/becareful/becarefulserver/domain/caregiver/service/CaregiverService.java
+++ src/main/java/com/becareful/becarefulserver/domain/caregiver/service/CaregiverService.java
@@ -157,6 +157,7 @@ public class CaregiverService {
                 request.nursingCareCertificate());
         caregiver.updateInfo(request.phoneNumber(), caregiverInfo);
     }
+
     public void logout(HttpServletResponse response) {
         response.addCookie(cookieUtil.deleteCookie("AccessToken"));
         response.addCookie(cookieUtil.deleteCookie("RefreshToken"));
