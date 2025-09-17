f275d29d040a58db8f331e540f88567d1f37ee84
Everdu <kckc0608@gmail.com>
2025-09-17 10:28:37 +0900

    refactor: 어르신 업데이트 로직 리팩토링 (#362)
    * refactor: 어르신 업데이트 로직 리팩토링
    
    * fix: 어르신 기본 프로필 이미지 링크로 수정
    
    * chore: spotless 적용

--- src/main/java/com/becareful/becarefulserver/domain/socialworker/dto/request/ElderlyUpdateRequest.java
+++ src/main/java/com/becareful/becarefulserver/domain/socialworker/dto/request/ElderlyUpdateRequest.java
@@ -5,19 +5,18 @@ import com.becareful.becarefulserver.domain.common.domain.Gender;
 import com.becareful.becarefulserver.domain.socialworker.domain.vo.CareLevel;
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 public record ElderlyUpdateRequest(
-        Optional<String> name,
-        Optional<LocalDate> birthday,
-        Optional<Boolean> inmate,
-        Optional<Boolean> pet,
-        Optional<Gender> gender,
-        Optional<CareLevel> careLevel,
-        Optional<String> siDo,
-        Optional<String> siGuGun,
-        Optional<String> eupMyeonDong,
-        Optional<String> detailAddress,
-        Optional<String> profileImageUrl,
-        Optional<String> healthCondition,
-        Optional<List<DetailCareType>> detailCareTypeList) {}
+        String name,
+        LocalDate birthday,
+        Boolean inmate,
+        Boolean pet,
+        Gender gender,
+        CareLevel careLevel,
+        String siDo, // TODO : Address VO 로 변경
+        String siGuGun,
+        String eupMyeonDong,
+        String detailAddress,
+        String profileImageUrl,
+        String healthCondition,
+        List<DetailCareType> detailCareTypeList) {}
