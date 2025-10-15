9623680de6c485fa0bb1f8f3fdd56bffc892a64b
kckc0608 <kckc0608@naver.com>
2025-10-15 22:12:34 +0900

    fix: enum 변경에 맞춰 쿼리 수정

--- src/main/java/com/becareful/becarefulserver/domain/matching/repository/RecruitmentRepository.java
+++ src/main/java/com/becareful/becarefulserver/domain/matching/repository/RecruitmentRepository.java
@@ -20,7 +20,7 @@ public interface RecruitmentRepository extends JpaRepository<Recruitment, Long> 
                    e,
                    COUNT(m.id),
                    COALESCE(
-                       SUM(CASE WHEN m.matchingStatus = com.becareful.becarefulserver.domain.matching.domain.MatchingStatus.지원검토중
+                       SUM(CASE WHEN m.matchingStatus = com.becareful.becarefulserver.domain.matching.domain.MatchingStatus.지원검토
                                 THEN 1
                                 ELSE 0
                            END),
@@ -43,7 +43,7 @@ public interface RecruitmentRepository extends JpaRepository<Recruitment, Long> 
                    e,
                    COUNT(m.id),
                    COALESCE(
-                       SUM(CASE WHEN m.matchingStatus = com.becareful.becarefulserver.domain.matching.domain.MatchingStatus.지원검토중
+                       SUM(CASE WHEN m.matchingStatus = com.becareful.becarefulserver.domain.matching.domain.MatchingStatus.지원검토
                                 THEN 1
                                 ELSE 0
                            END),
@@ -67,7 +67,7 @@ public interface RecruitmentRepository extends JpaRepository<Recruitment, Long> 
                    e,
                    COUNT(m.id),
                    COALESCE(
-                       SUM(CASE WHEN m.matchingStatus = com.becareful.becarefulserver.domain.matching.domain.MatchingStatus.지원검토중
+                       SUM(CASE WHEN m.matchingStatus = com.becareful.becarefulserver.domain.matching.domain.MatchingStatus.지원검토
                                 THEN 1
                                 ELSE 0
                            END),
