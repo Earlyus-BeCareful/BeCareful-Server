ea66b4cd58d80223bf69c5720348a1b3141e4acc
kckc0608 <kckc0608@naver.com>
2025-11-12 11:48:18 +0900

    docs: 일자리 지원서 표현 수정

--- src/main/java/com/becareful/becarefulserver/domain/caregiver/controller/CaregiverWorkApplicationController.java
+++ src/main/java/com/becareful/becarefulserver/domain/caregiver/controller/CaregiverWorkApplicationController.java
@@ -18,7 +18,7 @@ public class CaregiverWorkApplicationController {
 
     private final WorkApplicationService workApplicationService;
 
-    @Operation(summary = "일자리 신청 정보 등록/수정")
+    @Operation(summary = "일자리 지원서 등록/수정")
     @PutMapping
     public ResponseEntity<Void> createOrUpdateWorkApplication(
             @Valid @RequestBody WorkApplicationCreateOrUpdateRequest request) {
@@ -26,14 +26,14 @@ public class CaregiverWorkApplicationController {
         return ResponseEntity.ok().build();
     }
 
-    @Operation(summary = "일자리 신청 정보 조회", description = "요양보호사 일자리 신청서 정보를 조회합니다. 신청서를 등록한 적이 없다면 null을 반환합니다.")
+    @Operation(summary = "일자리 지원서 조회", description = "요양보호사 일자리 지원서를 조회합니다. 지원서를 등록한 적이 없다면 null을 반환합니다.")
     @GetMapping
     public ResponseEntity<CaregiverMyWorkApplicationPageResponse> getWorkApplication() {
         var response = workApplicationService.getMyWorkApplicationPageInfo();
         return ResponseEntity.ok(response);
     }
 
-    @Operation(summary = "일자리 신청 활성화", description = "등록한 일자리 신청 내용을 기반으로 매칭을 활성화 합니다.")
+    @Operation(summary = "일자리 신청 활성화", description = "등록한 일자리 지원서를 기반으로 매칭을 활성화 합니다.")
     @PostMapping("/active")
     public ResponseEntity<Void> updateWorkApplicationActive() {
         workApplicationService.updateWorkApplicationActive();
