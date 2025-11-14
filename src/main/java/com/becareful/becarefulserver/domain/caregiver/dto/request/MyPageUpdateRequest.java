09f5c715e9eb2151985fcf0e7a13f8ce7cab5f97
kckc0608 <kckc0608@naver.com>
2025-11-14 20:36:11 +0900

    refactor: mypage edit request 에 address 추가

--- src/main/java/com/becareful/becarefulserver/domain/caregiver/dto/request/MyPageUpdateRequest.java
+++ src/main/java/com/becareful/becarefulserver/domain/caregiver/dto/request/MyPageUpdateRequest.java
@@ -1,5 +1,6 @@
 package com.becareful.becarefulserver.domain.caregiver.dto.request;
 
+import com.becareful.becarefulserver.domain.caregiver.domain.vo.Address;
 import com.becareful.becarefulserver.domain.caregiver.domain.vo.Certificate;
 import jakarta.validation.constraints.NotNull;
 
@@ -9,5 +10,6 @@ public record MyPageUpdateRequest(
         @NotNull Certificate caregiverCertificate,
         Certificate socialWorkerCertificate,
         Certificate nursingCareCertificate,
+        Address address,
         boolean isHavingCar,
         boolean isCompleteDementiaEducation) {}
