fe30560541926554cc0031dc563b51a3269fbaab
kckc0608 <kckc0608@naver.com>
2025-11-15 00:41:56 +0900

    chore: spotless 적용

--- src/main/java/com/becareful/becarefulserver/domain/matching/dto/response/RecruitmentDetailResponse.java
+++ src/main/java/com/becareful/becarefulserver/domain/matching/dto/response/RecruitmentDetailResponse.java
@@ -3,11 +3,7 @@ package com.becareful.becarefulserver.domain.matching.dto.response;
 import com.becareful.becarefulserver.domain.matching.domain.Matching;
 import com.becareful.becarefulserver.domain.matching.domain.Recruitment;
 import com.becareful.becarefulserver.domain.matching.domain.vo.MatchingResultStatus;
-import com.becareful.becarefulserver.domain.matching.dto.ElderlyDto;
 import com.becareful.becarefulserver.domain.matching.dto.RecruitmentDto;
-import com.becareful.becarefulserver.domain.nursing_institution.domain.NursingInstitution;
-import com.becareful.becarefulserver.domain.nursing_institution.dto.InstitutionSimpleDto;
-import com.becareful.becarefulserver.domain.socialworker.domain.Elderly;
 
 public record RecruitmentDetailResponse(
         RecruitmentDto recruitmentInfo,
