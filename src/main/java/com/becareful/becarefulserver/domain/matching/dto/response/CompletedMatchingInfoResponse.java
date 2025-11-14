534c9179d8c1a38ab678151cc3ed72dd3cc3e13e
kckc0608 <kckc0608@naver.com>
2025-11-14 23:08:42 +0900

    refactor: 나의 일자리 아이템에 근무 시간 추가

--- src/main/java/com/becareful/becarefulserver/domain/matching/dto/response/CompletedMatchingInfoResponse.java
+++ src/main/java/com/becareful/becarefulserver/domain/matching/dto/response/CompletedMatchingInfoResponse.java
@@ -2,21 +2,30 @@ package com.becareful.becarefulserver.domain.matching.dto.response;
 
 import com.becareful.becarefulserver.domain.common.domain.CareType;
 import com.becareful.becarefulserver.domain.matching.domain.CompletedMatching;
+import com.becareful.becarefulserver.domain.matching.domain.Contract;
 import com.becareful.becarefulserver.domain.matching.dto.ElderlyDto;
 import com.becareful.becarefulserver.domain.socialworker.domain.Elderly;
 import java.time.DayOfWeek;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public record CompletedMatchingInfoResponse(
-        Long id, List<DayOfWeek> workDays, List<CareType> careTypes, String note, ElderlyDto elderlyInfo) {
+        Long id,
+        List<DayOfWeek> workDays,
+        String workStartTime,
+        String workEndTime,
+        List<CareType> careTypes,
+        String note,
+        ElderlyDto elderlyInfo) {
     public static CompletedMatchingInfoResponse from(CompletedMatching completedMatching) {
-        Elderly elderly =
-                completedMatching.getContract().getMatching().getRecruitment().getElderly();
-
+        Contract contract = completedMatching.getContract();
+        Elderly elderly = contract.getMatching().getRecruitment().getElderly();
         return new CompletedMatchingInfoResponse(
                 completedMatching.getId(),
-                completedMatching.getContract().getWorkDays().stream().toList(),
-                completedMatching.getContract().getCareTypes().stream().toList(),
+                contract.getWorkDays().stream().toList(),
+                contract.getWorkStartTime().format(DateTimeFormatter.ofPattern("HH:mm")),
+                contract.getWorkEndTime().format(DateTimeFormatter.ofPattern("HH:mm")),
+                contract.getCareTypes().stream().toList(),
                 completedMatching.getNote(),
                 ElderlyDto.from(elderly));
     }
