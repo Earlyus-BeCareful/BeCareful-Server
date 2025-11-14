b8bc8ede7627a330181fae11e4d551e252a656c2
kckc0608 <kckc0608@naver.com>
2025-11-15 01:01:47 +0900

    refactor: 불필요 클래스 제거

--- src/main/java/com/becareful/becarefulserver/domain/matching/dto/response/CaregiverAppliedRecruitmentsResponse.java
+++ src/main/java/com/becareful/becarefulserver/domain/matching/dto/response/CaregiverAppliedRecruitmentsResponse.java
@@ -3,16 +3,11 @@ package com.becareful.becarefulserver.domain.matching.dto.response;
 import com.becareful.becarefulserver.domain.matching.domain.Matching;
 import java.util.*;
 
-public record CaregiverAppliedRecruitmentsResponse(List<Item> recruitments, boolean hasNewChat) {
+public record CaregiverAppliedRecruitmentsResponse(
+        List<CaregiverRecruitmentResponse> recruitments, boolean hasNewChat) {
 
     public static CaregiverAppliedRecruitmentsResponse of(List<Matching> matchings, boolean hasNewChat) {
         return new CaregiverAppliedRecruitmentsResponse(
-                matchings.stream().map(Item::from).toList(), hasNewChat);
-    }
-
-    private record Item(CaregiverRecruitmentResponse recruitmentInfo) {
-        public static Item from(Matching matching) {
-            return new Item(CaregiverRecruitmentResponse.from(matching));
-        }
+                matchings.stream().map(CaregiverRecruitmentResponse::from).toList(), hasNewChat);
     }
 }
