f819523d5180398404312886ec1ec579532ffebf
kckc0608 <kckc0608@naver.com>
2025-11-18 22:46:26 +0900

    refactor: chat room last send message logic

--- src/main/java/com/becareful/becarefulserver/global/util/ChatUtil.java
+++ src/main/java/com/becareful/becarefulserver/global/util/ChatUtil.java
@@ -8,12 +8,18 @@ public class ChatUtil {
     public static String convertChatRoomListLastSendTimeFormat(LocalDateTime sendTime) {
         Duration duration = Duration.between(sendTime, LocalDateTime.now());
 
-        long hours = duration.toHours();
-        long minutes = duration.toMinutes();
         long days = duration.toDays();
-
-        if (hours < 1) return minutes + "분 전";
-        if (days < 1) return hours + "시간 전";
-        return days + "일 전";
+        if (days > 0) {
+            return days + "일 전";
+        }
+        long hours = duration.toHours();
+        if (hours > 0) {
+            return hours + "시간 전";
+        }
+        long minutes = duration.toMinutes();
+        if (minutes > 0) {
+            return minutes + "분 전";
+        }
+        return "방금 전";
     }
 }
