4a5497610dc64896efd7b13069a619a428dc48ea
Jiye Ryu <ryujy0716@gmail.com>
2025-12-03 18:14:49 +0900

    ChatSendRequest 역질렬화 타입 명시 (#506)
    * 역직ChatSendRequest 역질화 타입 명시
    
    * 재미나이 코드리뷰 반영

--- src/main/java/com/becareful/becarefulserver/domain/chat/dto/request/ChatSendRequest.java
+++ src/main/java/com/becareful/becarefulserver/domain/chat/dto/request/ChatSendRequest.java
@@ -1,7 +1,9 @@
 package com.becareful.becarefulserver.domain.chat.dto.request;
 
 import com.becareful.becarefulserver.domain.chat.domain.vo.ChatSendRequestType;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "sendRequestType")
 public sealed interface ChatSendRequest
         permits AcceptContractChatRequest, ConfirmContractChatRequest, EditContractChatRequest, SendTextChatRequest {
     ChatSendRequestType sendRequestType();
