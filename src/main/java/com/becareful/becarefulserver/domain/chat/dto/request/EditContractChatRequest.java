0afdcd9d8c6d8b0ffe5b4fc5fdfa77e0286d2f4d
campus2 H056 <ryujy0716@gmail.com>
2025-12-03 20:19:03 +0900

    ChatSendRequest 인스턴스에 서브타입 명시

--- src/main/java/com/becareful/becarefulserver/domain/chat/dto/request/EditContractChatRequest.java
+++ src/main/java/com/becareful/becarefulserver/domain/chat/dto/request/EditContractChatRequest.java
@@ -3,7 +3,6 @@ package com.becareful.becarefulserver.domain.chat.dto.request;
 import com.becareful.becarefulserver.domain.caregiver.domain.WorkSalaryUnitType;
 import com.becareful.becarefulserver.domain.chat.domain.vo.ChatSendRequestType;
 import com.becareful.becarefulserver.domain.common.domain.CareType;
-import com.fasterxml.jackson.annotation.JsonTypeName;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.time.DayOfWeek;
@@ -11,7 +10,6 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
-@JsonTypeName("EDIT_CONTRACT")
 public record EditContractChatRequest(
         ChatSendRequestType sendRequestType,
         @Size(min = 1) List<DayOfWeek> workDays,
--- src/main/java/com/becareful/becarefulserver/domain/chat/dto/request/SendTextChatRequest.java
+++ src/main/java/com/becareful/becarefulserver/domain/chat/dto/request/SendTextChatRequest.java
@@ -1,7 +1,5 @@
 package com.becareful.becarefulserver.domain.chat.dto.request;
 
 import com.becareful.becarefulserver.domain.chat.domain.vo.ChatSendRequestType;
-import com.fasterxml.jackson.annotation.JsonTypeName;
 
-@JsonTypeName("SEND_TEXT")
 public record SendTextChatRequest(ChatSendRequestType sendRequestType, String text) implements ChatSendRequest {}
