46719fce3688e51f86b428f27720bc9d6a62da63
campus2 H056 <ryujy0716@gmail.com>
2025-12-05 16:27:11 +0900

    log 라이브러리 변경

--- src/main/java/com/becareful/becarefulserver/global/security/JwtAuthenticationFilter.java
+++ src/main/java/com/becareful/becarefulserver/global/security/JwtAuthenticationFilter.java
@@ -2,7 +2,6 @@ package com.becareful.becarefulserver.global.security;
 
 import static com.becareful.becarefulserver.global.exception.ErrorMessage.INVALID_REFRESH_TOKEN;
 
-import com.becareful.becarefulserver.domain.auth.handler.CustomSuccessHandler;
 import com.becareful.becarefulserver.global.constant.SecurityConstant;
 import com.becareful.becarefulserver.global.exception.ErrorMessage;
 import com.becareful.becarefulserver.global.exception.exception.AuthException;
@@ -19,8 +18,6 @@ import java.util.ArrayList;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -34,7 +31,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtUtil jwtUtil;
     private final JwtProperties jwtProperties;
     private final CookieProperties cookieProperties;
-    private static final Logger logger = LoggerFactory.getLogger(CustomSuccessHandler.class);
 
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) {
@@ -80,7 +76,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
                 response.addCookie(newAccessTokenCookie);
             } else if (accessToken == null && refreshToken == null) {
-                logger.info("URI: {}", request.getRequestURI()); // 어떤 요청에서 에러가 발생한건지 확인
+                log.info("URI: {}", request.getRequestURI()); // 어떤 요청에서 에러가 발생한건지 확인
                 throw new AuthException(ErrorMessage.TOKEN_NOT_CONTAINED);
             } else {
                 throw new AuthException(INVALID_REFRESH_TOKEN);
