e58c4b92beeeca50fa071e6b08948c0e396f63ae
Jiye Ryu <ryujy0716@gmail.com>
2025-12-06 20:36:57 +0900

    [웹소켓] 로그 추가 (#515)
    * ãlocal 작업
    
    * local ì 작업 임시커밋
    
    * 채팅 웹소켓 연결 구현
    
    * 채팅 웹소켓 연결 구현
    
    * gmini 코드 리뷰 반영
    
    * handshake 핸들러 추가
    
    * 웹소켓 핸드셰이크 jwtAuthenticationFilter 거치도록 수정
    
    * 로그 추가
    
    * spotless 적용

--- src/main/java/com/becareful/becarefulserver/global/websocket/JwtHandshakeHandler.java
+++ src/main/java/com/becareful/becarefulserver/global/websocket/JwtHandshakeHandler.java
@@ -2,22 +2,28 @@ package com.becareful.becarefulserver.global.websocket;
 
 import java.security.Principal;
 import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeFailureException;
 import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
+@Slf4j
 @Component
 public class JwtHandshakeHandler extends DefaultHandshakeHandler {
     @Override
     protected Principal determineUser(
             @NotNull ServerHttpRequest request, @NotNull WebSocketHandler wsHandler, Map<String, Object> attributes) {
-        Object principal = attributes.get("PRINCIPAL");
+        log.info("웹소켓 handshakerHandler 시작"); // TODO(에러 확인용. 삭제 예정)
 
-        if (principal instanceof Principal P) {
+        Object principal = attributes.get("principal");
+
+        if (principal instanceof ChatPrincipal P) {
+            log.info("웹소켓 handshakeHandler 통과"); // TODO(에러 확인용. 삭제 예정)
             return P;
         }
-        return null;
+        throw new HandshakeFailureException("웹소켓 handshakeHandler 실패. 잘못된 principal 형식 입니다.");
     }
 }
