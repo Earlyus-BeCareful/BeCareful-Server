515362dba96f42eb69c5308b14b963dd4296f463
Jiye Ryu <ryujy0716@gmail.com>
2025-12-11 21:35:19 +0900

    Refactor/456 application (#523)
    * feat: Application Entity 정의
    
    * feat: 지원 데이터 생성 로직 작성
    
    * refactor: 매칭 조회로직 리팩토링
    
    * refactor: 매칭 적합도 계산 로직 리팩토링
    
    * refactor: 사회복지사 매칭 로직 리팩토링
    
    * refactor: 요양보호사 매칭 로직 리팩토링
    
    * refactor: 사회복지사 채팅 로직 리팩토링
    
    * refactor: 요양보호사 매칭 통계 로직 리팩토링
    
    * refactor: Matching 엔티티 제거
    
    * fix: jpql 수정
    
    * refactor: matching domain service 리팩토링
    
    * refactor: 매칭 판별 로직 래핑
    
    * refactor: jpql 최적화
    
    * fix: 채용완료시 공고 상태 변경 로직 추가
    
    * refactor: 쿼리 최적화
    
    * chore: spotless 적용
    
    * 요양보호사 일자리 화면에 이름 추가
    
    ---------
    
    Co-authored-by: kckc0608 <kckc0608@naver.com>

--- src/main/java/com/becareful/becarefulserver/domain/caregiver/dto/response/CaregiverMyWorkApplicationPageResponse.java
+++ src/main/java/com/becareful/becarefulserver/domain/caregiver/dto/response/CaregiverMyWorkApplicationPageResponse.java
@@ -2,8 +2,10 @@ package com.becareful.becarefulserver.domain.caregiver.dto.response;
 
 import com.becareful.becarefulserver.domain.caregiver.dto.*;
 
-public record CaregiverMyWorkApplicationPageResponse(boolean hasCareer, WorkApplicationDto workApplicationDto) {
-    public static CaregiverMyWorkApplicationPageResponse of(boolean hasCareer, WorkApplicationDto workApplicationDto) {
-        return new CaregiverMyWorkApplicationPageResponse(hasCareer, workApplicationDto);
+public record CaregiverMyWorkApplicationPageResponse(
+        String caregiverName, boolean hasCareer, WorkApplicationDto workApplicationDto) {
+    public static CaregiverMyWorkApplicationPageResponse of(
+            String caregiverName, boolean hasCareer, WorkApplicationDto workApplicationDto) {
+        return new CaregiverMyWorkApplicationPageResponse(caregiverName, hasCareer, workApplicationDto);
     }
 }
