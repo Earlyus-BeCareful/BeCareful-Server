43bf32ac34db057df4fe15c11b97f3a8440384da
kckc0608 <kckc0608@naver.com>
2025-12-28 12:11:36 +0900

    feat: 신고 대상 외래키 추가

--- src/main/java/com/becareful/becarefulserver/domain/report/domain/Report.java
+++ src/main/java/com/becareful/becarefulserver/domain/report/domain/Report.java
@@ -1,6 +1,9 @@
 package com.becareful.becarefulserver.domain.report.domain;
 
+import com.becareful.becarefulserver.domain.chat.domain.ChatRoom;
 import com.becareful.becarefulserver.domain.common.domain.BaseEntity;
+import com.becareful.becarefulserver.domain.community.domain.Comment;
+import com.becareful.becarefulserver.domain.community.domain.Post;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -22,4 +25,16 @@ public class Report extends BaseEntity {
 
     @Column(name = "report_description", length = 500)
     private String description;
+
+    @JoinColumn(name = "reported_post_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Post post;
+
+    @JoinColumn(name = "reported_comment_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Comment comment;
+
+    @JoinColumn(name = "reported_chat_room_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private ChatRoom chatRoom;
 }
