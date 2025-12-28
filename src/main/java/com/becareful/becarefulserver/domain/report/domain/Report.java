7772a1f0504be647fabd94a0d9fcbd59701de7d5
Everdu <kckc0608@gmail.com>
2025-12-28 12:18:50 +0900

    feat: create report entity (#539)
    * feat: create report entity
    
    * chore: spotless 적용
    
    * chore: 컬럼 설정 추가
    
    * feat: 신고 대상 외래키 추가
    
    * feat: 신고자 외래키 추가

---
+++ src/main/java/com/becareful/becarefulserver/domain/report/domain/Report.java
@@ -0,0 +1,50 @@
+package com.becareful.becarefulserver.domain.report.domain;
+
+import com.becareful.becarefulserver.domain.caregiver.domain.Caregiver;
+import com.becareful.becarefulserver.domain.chat.domain.ChatRoom;
+import com.becareful.becarefulserver.domain.common.domain.BaseEntity;
+import com.becareful.becarefulserver.domain.community.domain.Comment;
+import com.becareful.becarefulserver.domain.community.domain.Post;
+import com.becareful.becarefulserver.domain.socialworker.domain.SocialWorker;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Report extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "report_id")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "report_type", nullable = false)
+    private ReportType reportType;
+
+    @Column(name = "report_description", length = 500)
+    private String description;
+
+    @JoinColumn(name = "reported_post_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Post post;
+
+    @JoinColumn(name = "reported_comment_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Comment comment;
+
+    @JoinColumn(name = "reported_chat_room_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private ChatRoom chatRoom;
+
+    @JoinColumn(name = "reporter_caregiver_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Caregiver caregiver;
+
+    @JoinColumn(name = "reporter_social_worker_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private SocialWorker socialWorker;
+}
