9a049416ec3b303020e24460a7e6d9b7c967b6d9
kckc0608 <kckc0608@naver.com>
2025-12-29 21:49:16 +0900

    chore: 불필요 필드 request dto 에서 삭제

--- src/main/java/com/becareful/becarefulserver/domain/report/dto/request/ReportCreateRequest.java
+++ src/main/java/com/becareful/becarefulserver/domain/report/dto/request/ReportCreateRequest.java
@@ -2,16 +2,5 @@ package com.becareful.becarefulserver.domain.report.dto.request;
 
 import com.becareful.becarefulserver.domain.report.domain.ReportType;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 
-public record ReportCreateRequest(
-        @NotNull ReportTarget reportTarget,
-        @NotNull @Positive Long reportTargetId,
-        @NotNull ReportType reportType,
-        String description) {
-    public enum ReportTarget {
-        POST,
-        COMMENT,
-        CHATROOM
-    }
-}
+public record ReportCreateRequest(@NotNull ReportType reportType, String description) {}
